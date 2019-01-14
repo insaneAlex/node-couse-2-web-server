66b45d620a913c2abf9980af010ad5227fcd41f5
sassha96 <syvoplias76@gmail.com>
2019-01-14 02:33:25 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs')
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    let log = `${now}:${req.method}:${req.url}`;
+    //console.log(log);
+
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append file to server.log');
+        }
+    });
+    next();
+})
+
+// app.use('/about',(req, res, next) => {
+//     res.render('maintenance.hbs')
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        welcomeMessage: 'Welcome to my site',
+        pageTitle: 'Home Page',
+        linkArrdess: '/about',
+        linkText: 'About Page'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+
+        linkArrdess: '/',
+        linkText: 'Home Page'
+    });
+})
+
+app.listen(2000, () => {
+    console.log('Running server...');
+});
