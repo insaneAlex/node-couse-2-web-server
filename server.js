fc48e7c39f65c82ab13da38da2051637adbbcb5f
insaneAlex <sasha.syvoplias@gmail.com>
2019-01-14 04:16:25 +0200

    Added heroku support

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs')
 
+const port = process.env.PORT || 2000;
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -54,6 +55,6 @@ app.get('/about', (req, res) => {
     });
 })
 
-app.listen(2000, () => {
-    console.log('Running server...');
+app.listen(port, () => {
+    console.log(`Running server on ${port}...`);
 });
